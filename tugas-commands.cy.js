4509afcfbecd1f41eb020ab07fc472b606b0b43c
rinapriani <rina.apriani498@gmail.com>
2022-09-11 20:20:59 +0700

    tugas 5 commands

---
+++ tugas-commands.cy.js
@@ -0,0 +1,45 @@
+/// <reference types ='cypress' />
+
+describe('Working with input', () => {
+    it('visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include','login.html')
+    });
+    //it('Should fill username', () => {
+        //cy.get('#user_login').clear()
+        //cy.get('#user_login').type(username)
+    //});
+    //it('Should fill password', () => {
+        //cy.get('input[name = "user_password"]').clear()
+        //cy.get('input[name = "user_password"]').type(password)
+    //});
+
+    it('Should try to login', () => {
+        cy.fixture("test-login").then(user => {
+            const username =user.username
+            const password = user.password
+
+            //penambahan command.js
+            cy.login(username,password)
+
+            cy.url().should('include', '/bank/account-summary.html')
+
+            cy.get('#pay_bills_tab').click()
+
+        });
+    });
+
+    it('Should complete payment', () => {
+        cy.fixture("bank-bills").then(complete => {
+            const payee = complete.payee
+            const account = complete.account
+            const amount = complete.amount
+            const date = complete.date
+            const description = complete.description
+
+            cy.bills(payee,account,amount,date,description)
+
+        })
+    });
+    
+});
